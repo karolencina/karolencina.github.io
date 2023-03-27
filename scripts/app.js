7ef2c56d5e2063197c2acf8a3dd9cec40597e2bc
Karolina Lencina <karolencina@icloud.com>
2023-03-27 23:57:47 +0100

    Add scrolling to sections

--- scripts/app.js
+++ scripts/app.js
@@ -5,4 +5,18 @@ const navbarHeight = navbar.offsetHeight;
 
 // Set the height of the #home section to the remaining viewport height
 const homeSection = document.querySelector('#home');
-homeSection.style.height = `calc(100vh - ${navbarHeight}px)`;
+homeSection.style.height = `calc(100vh - ${navbarHeight}px - ${homeSection.style.paddingTop} - ${homeSection.style.paddingBottom})`;
+
+// Smooth scrolling for all links with the anchor attribute
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        window.scrollTo({
+            top: target.offsetTop - navbarHeight,
+            behavior: 'smooth'
+        });
+    });
+});
